4b16df6593fbb75c3ceb0342c7b0b84f52509d84
stolenusb <xxuber6@gmail.com>
2026-08-04 14:15:59 +0100

    Ran cs:fix

--- backend/src/Entity/Trip.php
+++ backend/src/Entity/Trip.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\ApiFilter;
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
-use App\Controller\TripCollectionController;
 use ApiPlatform\OpenApi\Model\Operation;
 use ApiPlatform\OpenApi\Model\Response;
+use App\Controller\TripCollectionController;
 use App\Controller\TripStatusController;
 use App\Enum\ReservationStatus;
 use App\Enum\TripStatus;
